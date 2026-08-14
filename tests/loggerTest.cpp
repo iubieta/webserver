9d47426605886a0fdf15a0d8d79f89eabe8e6a76
Iker Ubieta <ikerp.ubieta@gmail.com>
2026-08-14 19:35:01 +0200

    test (logger): file error, console level

--- tests/loggerTest.cpp
+++ tests/loggerTest.cpp
@@ -4,11 +4,17 @@
 // ----------------------------------------------------------------------------
 
 #include <fstream>
+#include <sstream>
+#include <streambuf>
 #include <string>
 #include <cstdio>
+#include <strings.h>
 
 #include "Tester.hpp"
 #include "../inc/Logger.hpp"
+
+// Auxiliar methods
+// ----------------------------------------------------------------------------
 
 void	logEveryLevel(Logger &log) {
 	log.debug("Debug message");
@@ -32,8 +38,47 @@ int		numberOfLines(const std::string &filepath) {
 	return count;
 }
 
-void	checkFilter(Logger::LogLevel level, int expected) {
-	std::string	logfile = "levelFiltering.log";
+std::streambuf	*redirCerr(std::ostringstream &out) {
+	std::streambuf *ogStream = std::cerr.rdbuf();
+	std::streambuf *newStream = out.rdbuf();
+	std::cerr.rdbuf(newStream);
+	return ogStream;
+}
+
+void	resetCerr(std::streambuf *ogStream) {
+	std::cerr.rdbuf(ogStream);
+}
+
+int		countCharInStr(char c, const std::string str) {
+	size_t i = 0;
+	int count = 0;
+	i = str.find_first_of(c, i);
+	while (i != std::string::npos) {
+		++count;
+		i = str.find_first_of(c, ++i);
+	}
+	return count;
+}
+
+// Test 1: File opening error
+// ----------------------------------------------------------------------------
+void	testFileError() {
+	std::string	logfile = "/unknown/path/to/file.log";
+
+	std::ostringstream consoleOut;
+	std::streambuf *ogStream = redirCerr(consoleOut);
+	Logger	log(logfile);
+	resetCerr(ogStream);
+
+	std::string out = consoleOut.str();
+	std::string expected = "[WARNING] Log file could not be opened, continuing in console mode\n";
+	ASSERT_EQ(out, expected);
+}
+
+// Test 2: File Level Filtering
+// ----------------------------------------------------------------------------
+void	testFileLevel(Logger::LogLevel level, int expected) {
+	std::string	logfile = "temp.log";
 	std::remove(logfile.c_str());
 	int lines;
 	{
@@ -48,15 +93,43 @@ void	checkFilter(Logger::LogLevel level, int expected) {
 	std::remove(logfile.c_str());
 }
 
-void	testLevelFiltering() {
-	checkFilter(Logger::DEBUG, 5);
-	checkFilter(Logger::INFO, 4);
-	checkFilter(Logger::WARNING, 3);
-	checkFilter(Logger::ERROR, 2);
-	checkFilter(Logger::CRITICAL, 1);
+
+void	testFileLevelFiltering() {
+	testFileLevel(Logger::DEBUG, 5);
+	testFileLevel(Logger::INFO, 4);
+	testFileLevel(Logger::WARNING, 3);
+	testFileLevel(Logger::ERROR, 2);
+	testFileLevel(Logger::CRITICAL, 1);
 }
 
+// Test 3: Console Level Filtering
+// ----------------------------------------------------------------------------
+void	testConsoleLevel(Logger::LogLevel level, int expected) {
+	
+	std::ostringstream consoleOut;
+	std::streambuf *ogStream = redirCerr(consoleOut);
+	Logger	log;
+	log.setConsoleLevel(level);
+	logEveryLevel(log);
+	resetCerr(ogStream);
+	int lines = 0;
+	lines = countCharInStr('\n', consoleOut.str());
+	ASSERT_EQ(lines, expected);
+}
+
+void	testConsoleLevelFiltering() {
+	testConsoleLevel(Logger::DEBUG, 5);
+	testConsoleLevel(Logger::INFO, 4);
+	testConsoleLevel(Logger::WARNING, 3);
+	testConsoleLevel(Logger::ERROR, 2);
+	testConsoleLevel(Logger::CRITICAL, 1);
+}
+
+// Test battery
+// ----------------------------------------------------------------------------
 int main() {
-	Tester::runTest("Level filtering", testLevelFiltering);
+	Tester::runTest("File error", testFileError);
+	Tester::runTest("File Level filtering", testFileLevelFiltering);
+	Tester::runTest("Console Level filtering", testConsoleLevelFiltering);
 	return Tester::report();
 }
