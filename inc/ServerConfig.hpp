aa52032111802f74f2c1cfd4f43c7290b80a3244
Yuliano96 <yulianopacileo@gmail.com>
2026-08-17 19:57:53 +0200

    wip (config): starting with parse header

--- inc/ServerConfig.hpp
+++ inc/ServerConfig.hpp
@@ -15,18 +15,39 @@
 
 #include "LocationConfig.hpp"
 
-class	ServerConfig {
+class	ServerConfig 
+{
 	private:
-		uint16_t							listen_port;
-		uint32_t 							host;
-		std::string							server_name;
-		struct sockaddr_in					server_addr;
-		std::string							root;
-		size_t								client_max_body_size;
-		bool								autoindex;
-		std::string							index;
-		std::map<short, std::string>		error_pages;
-		std::vector<LocationConfig>			locations;
+		unsigned int						listen_;
+		unsigned int						host_;
+		std::string							server_name_;
+		std::string							root_;
+		size_t								client_max_body_size_;
+		bool								autoindex_;
+		std::vector<std::string>			index_;
+		std::vector<LocationConfig>			locations_;
+		//std::map<int, std::string>			error_pages_;
+
+	public:
+		ServerConfig();
+		ServerConfig(const ServerConfig &other);
+		ServerConfig &operator=(const ServerConfig &other);
+		~ServerConfig();
+
+		const unsigned int &getListen() const;
+		const unsigned int &getHost() const;
+		const std::string &getServerName() const;
+		const std::string &getRoot() const;
+		const size_t &getClientMaxBody() const;
+		const bool &getAutoindex() const;
+
+		void setListen(unsigned int listen);
+		void setHost(unsigned int host);
+		void setServerName(const std::string &server_name);
+		void setRoot(const std::string &root);
+		void setClientMax(const size_t client_max_body);
+		void setAutoindex(bool autoindex);
+
 };
 
 #endif // !SERVER_CONFIG_HPP
