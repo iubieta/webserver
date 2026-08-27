d96a230d0602f752b452eb6cdea23ca8ce8f7445
Iker Ubieta <ikerp.ubieta@gmail.com>
2026-08-27 19:41:47 +0200

    feat (server core): Connection class

---
+++ inc/Connection.hpp
@@ -0,0 +1,36 @@
+// Connection.hpp
+// ----------------------------------------------------------------------------
+
+#ifndef CONNECTION_HPP
+#define CONNECTION_HPP
+
+#include <cstddef>
+#include <string>
+#include <sys/types.h>
+
+#define BUFFLEN 2048
+
+class Connection {
+private:
+	int				fd_;
+	bool			closed_;
+	std::string		read_buff_;
+	std::string		write_buff_;
+	size_t			write_offset_;
+
+	Connection(const Connection &other);
+	Connection& operator=(const Connection &other);
+
+public:
+	Connection(int fd);
+	~Connection();
+
+	ssize_t		readFromFd();
+	ssize_t		writeToFd();
+	void		appendToWriteBuff(const std::string &data); // Used in response builder
+	void		disconnect();
+	bool		wantsWrite() const;	// There is data on the write buffer
+	bool		isClosed() const;	// Client closed the connection
+};
+
+#endif
---
+++ src/Connection.cpp
@@ -0,0 +1,59 @@
+// Connection.cpp
+// ----------------------------------------------------------------------------
+
+#include "../inc/Connection.hpp"
+#include <sys/socket.h>
+#include <unistd.h>
+
+// Constructors ---------------------------------------------------------------
+Connection::Connection(int fd) : fd_(fd), closed_(false), write_offset_(0) 
+{}
+
+Connection::~Connection() {
+	if (fd_ != -1) {
+		close(fd_);
+	}
+}
+
+// Public methods -------------------------------------------------------------
+ssize_t Connection::readFromFd() {
+	if (fd_ == -1) {
+		return -1;
+	}
+	char	temp[BUFFLEN];
+	ssize_t read_bytes = recv(fd_, temp, BUFFLEN, 0);
+	if (read_bytes > 0) {
+		read_buff_.append(temp, read_bytes);
+	}
+	return read_bytes;
+}
+
+ssize_t Connection::writeToFd() {
+	if (write_offset_ >= write_buff_.size())
+		return 0;
+	const char *temp = write_buff_.c_str() + write_offset_;
+	ssize_t sent_bytes = send(fd_, temp, BUFFLEN, 0);
+	if (sent_bytes > 0)
+		write_offset_ += sent_bytes;
+	return sent_bytes;
+}
+
+void Connection::appendToWriteBuff(const std::string &data) {
+	write_buff_.append(data);
+}
+
+void Connection::disconnect() {
+	if (fd_ != -1) {
+		close(fd_);
+		fd_ = -1;
+	}
+	closed_ = true;
+}
+
+bool Connection::wantsWrite() const {
+	return write_offset_ < write_buff_.size();
+}
+
+bool Connection::isClosed() const {
+	return closed_;
+}
